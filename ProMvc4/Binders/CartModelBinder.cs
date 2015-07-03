68ff9eec65873d7bb09a7a82ddc8fe2db86cdd6f
binlyzhuo <binlyzhuo@outlook.com>
2015-07-03 17:46:10 +0800

    ~commit codes

---
+++ ProMvc4/Binders/CartModelBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProMvc4.Binders
+{
+    public class CartModelBinder:IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            return null;
+        }
+    }
+}
