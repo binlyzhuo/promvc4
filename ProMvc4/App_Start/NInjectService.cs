b88dc87cf629d54f43b6a4b5c59d2fd9d78a685f
binlyzhuo <binlyzhuo@outlook.com>
2015-07-03 15:20:19 +0800

    ~commit

---
+++ ProMvc4/App_Start/NInjectService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProMvc4.App_Start
+{
+    public class NInjectService:IDependencyResolver
+    {
+        public object GetService(Type serviceType)
+        {
+            return null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return null;
+        }
+    }
+}
