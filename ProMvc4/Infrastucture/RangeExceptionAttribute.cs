19f03bf3500c3e71e69a7fc62f544667ce6953fb
binlyzhuo <binlyzhuo@outlook.com>
2015-07-06 13:42:47 +0800

    ~commit

--- ProMvc4/Infrastucture/RangeExceptionAttribute.cs
+++ ProMvc4/Infrastucture/RangeExceptionAttribute.cs
@@ -6,13 +6,21 @@ using System.Web.Mvc;
 
 namespace ProMvc4.Infrastucture
 {
-    public class RangeExceptionAttribute:FilterAttribute,IExceptionFilter
+    public class RangeExceptionAttribute : FilterAttribute, IExceptionFilter
     {
         public void OnException(ExceptionContext filterContext)
         {
-            if(!filterContext.ExceptionHandled&&filterContext.Exception is ArgumentOutOfRangeException)
+            if (!filterContext.ExceptionHandled && filterContext.Exception is ArgumentOutOfRangeException)
             {
-                filterContext.Result = new RedirectResult("~/Common/RangeErrorPage.html");
+                //filterContext.Result = new RedirectResult("~/Common/RangeErrorPage.html");
+
+                int val = (int)((ArgumentOutOfRangeException)filterContext.Exception).ActualValue;
+
+                filterContext.Result = new ViewResult
+                {
+                    ViewName = "RangeError",
+                    ViewData = new ViewDataDictionary<int>(val)
+                };
                 filterContext.ExceptionHandled = true;
             }
         }
