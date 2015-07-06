a5a3517068d9fbc6274337c4658b3eca9e935a2d
binlyzhuo <binlyzhuo@outlook.com>
2015-07-06 16:16:34 +0800

    ~commit api

---
+++ ProMvc4/Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProMvc4.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { set; get; }
+        public string ClientName { set; get; }
+        public string Location { set; get; }
+    }
+}
