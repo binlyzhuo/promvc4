5052d6d5419d927803e183aa0fee4fae488406ff
binlyzhuo <binlyzhuo@outlook.com>
2015-07-06 14:34:25 +0800

    ~commit ProMvc4.csproj

---
+++ ProMvc4/Infrastucture/ProfileResultAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ProMvc4.Infrastucture
+{
+    public class ProfileResultAttribute:FilterAttribute,IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+
+        }
+    }
+}
