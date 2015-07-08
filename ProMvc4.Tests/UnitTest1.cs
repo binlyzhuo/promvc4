81ff24a155d0b26f2a9467133a305a60a43fdea7
binlyzhuo <binlyzhuo@outlook.com>
2015-07-08 09:58:29 +0800

    ~commit test project

---
+++ ProMvc4.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProMvc4.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
